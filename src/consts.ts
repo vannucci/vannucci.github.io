fce5fcbec8d5482409edf62989c553e0c79ffb4a
Michael Vannucci <michael.vannucci@pm.me>
2026-08-17 12:17:08 -0400

    Basic changes

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,3 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = "Mike Vannucci's Blog";
-export const SITE_DESCRIPTION = "Welcome, nerds!";
+export const SITE_TITLE = "Mike Vannucci";
+export const SITE_DESCRIPTION =
+  "On learning distributed systems, and what it does to you.";
